7d4616b4f358765a69848bfe6b6607a43f1ab203
mfhsohag01 <md.sohag2001bd@gmail.com>
2025-06-11 00:28:54 +0600

    feat: midnight breakthrough — it finally works �✨

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import connectDB from "./configs/db.js";
-import ClerkWebhooks from "./controllers/ClerkWebHooks.js";
+import ClerkWebhooks from "./controllers/ClerkWebhooks.js";
 
 connectDB();
 
@@ -20,5 +20,4 @@ app.use("/api/clerk", ClerkWebhooks);
 app.get("/", (req, res) => res.send("API is working"));
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
